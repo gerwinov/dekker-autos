3bfe5adc095e92596e9d8049a37be7c46c1c2b18
Gerwin Overeem <gerwin@overeem.io>
2022-02-03 15:15:58 +0100

    Content and layout changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["./dist/index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./dist/index.html"],
+	theme: {
+		extend: {
+			colors: {
+				'da-blue': '#188ef4',
+				'da-blue-darken': '#1770bc',
+				'da-orange': '#ffb15b',
+				'da-orange-darken': '#f58028',
+			}
+		},
+	},
+	plugins: [],
 }
